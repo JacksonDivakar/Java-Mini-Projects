4529cf101de1978eb79725ea9925c368e97f3285
Jackson Divakar <63591116+JacksonDivakar@users.noreply.github.com>
2025-05-08 15:10:53 +0530

    train_reservation_system

---
+++ Train_Booking_Reservation_System/train_reservation_system.java
@@ -0,0 +1,12 @@
+package Train_Reservation_System;
+
+import java.util.*;
+
+public class train_reservation_system {
+    public static void main(String[] args) {
+        booking bk = new booking();
+        booking.getBookingDetails();
+        booking bk_1 = new booking();
+        booking.getBookingDetails();
+    }
+}
